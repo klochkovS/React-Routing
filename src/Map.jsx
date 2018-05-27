f182204c7b90aa6d992c9286be7f375df0cf4d3b
klochkovS <fluurr@gmail.com>
2018-05-27 18:54:11 +0300

    fix

--- src/Map.jsx
+++ src/Map.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Map = () => (
   <div>
     <h1>Map</h1>
-    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A689728f02903bce87c79be65e5ac90ad5da51a156f1536d7859471244abc10c0&amp;source=constructor" width="500" height="400" frameborder="0"></iframe>
+    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A689728f02903bce87c79be65e5ac90ad5da51a156f1536d7859471244abc10c0&amp;source=constructor" width="500" height="400" frameBorder="0"></iframe>
   </div>
 );
 
