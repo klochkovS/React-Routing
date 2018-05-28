159776531d62434f0cd018f935a43865993ea80a
klochkovS <fluurr@gmail.com>
2018-05-28 16:46:51 +0300

    some code style fix

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,23 +1,6 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     stars: props.stars,
-  //   };
-  // }
-
-  plus() {
-    const stars = +this.state.stars < 10 ? +this.state.stars + 1 : 10;
-    this.setState({ stars });
-  }
-
-  minus() {
-    const stars = +this.state.stars > 1 ? +this.state.stars - 1 : 1;
-    this.setState({ stars });
-  }
 
   render() {
     const { stars, addStar, subStar } = this.props;
